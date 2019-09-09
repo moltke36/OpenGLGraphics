4b83a58be8c3fd531c0141290fe7e2afba6a9732
moltke36 <516503324@qq.com>
2019-09-09 01:38:24 -0600

    Depth Test

--- OpenGLGraphics/MyGLWindow.cpp
+++ OpenGLGraphics/MyGLWindow.cpp
@@ -1,25 +1,29 @@
-#include <GL\glew.h>
+ï»¿#include <GL\glew.h>
 #include <iostream>
+#include <fstream>
 #include "MyGLWindow.h"
 
-extern const char* vertexShaderCode;
-extern const char* fragmentShaderCode;
 
 void MyGLWindow::sendDataToOpenGL()
 {
+	const float RED_TRIANGLE_Z = 0.5f;
+	const float BLUE_TRIANBLE_Z = -0.5f;
 
 	// Create verts
 	GLfloat verts[] =
 	{
-		+0.0f,+0.0f,
+		-1.0f,-1.0f, RED_TRIANGLE_Z,
 		+1.0f, +0.0f, +0.0f,
-		+1.0f,+1.0f,
-		+0.0f, +1.0f, +0.0f,
-		-1.0f,+1.0f,
+		+0.0f,+1.0f, -1.0f,
+		+1.0f, +1.0f, +1.0f,
+		+1.0f,-1.0f, RED_TRIANGLE_Z,
+		+1.0f, +0.0f, +0.0f,
+
+		-1.0f,+1.0f,BLUE_TRIANBLE_Z,
 		+0.0f, +0.0f, +1.0f,
-		-1.0f,-1.0f,
-		+0.0f, +1.0f, +0.0f,
-		+1.0f,-1.0f,
+		+0.0f,-1.0f,BLUE_TRIANBLE_Z,
+		+0.0f, +0.0f, +1.0f,
+		+1.0f,+1.0f,BLUE_TRIANBLE_Z,
 		+0.0f, +0.0f, +1.0f
 	};
 
@@ -27,23 +31,23 @@ void MyGLWindow::sendDataToOpenGL()
 	GLuint vertexBufferID;
 	// Create a Buffer Object with myID
 	glGenBuffers(1, &vertexBufferID);
-	// ±ØĞëÒªÑ¡Ò»¸öBufferÀàĞÍ°ó¶¨£¬ELEMENT_ARRAY OR ARRAY
-	//£¨»òĞí¿ÉÒÔÀí½â³ÉBuffer objectÁ¬½ÓÉÏARRAY£©
+	// å¿…é¡»è¦é€‰ä¸€ä¸ªBufferç±»å‹ç»‘å®šï¼ŒELEMENT_ARRAY OR ARRAY
+	//ï¼ˆæˆ–è®¸å¯ä»¥ç†è§£æˆBuffer objectè¿æ¥ä¸ŠARRAYï¼‰
 	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
-	// Send Data to Buffer(ÄÄ¸öbuff£¬data´óĞ¡£¬data, ËÍµ½ÄÄÀï£©
+	// Send Data to Buffer(å“ªä¸ªbuffï¼Œdataå¤§å°ï¼Œdata, é€åˆ°å“ªé‡Œï¼‰
 	glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
-	// Enable Vertex Attribute (vertexÓĞºÜ¶à²»Í¬ÊôĞÔ£¬Î»ÖÃÊÇÒ»¸ö);
+	// Enable Vertex Attribute (vertexæœ‰å¾ˆå¤šä¸åŒå±æ€§ï¼Œä½ç½®æ˜¯ä¸€ä¸ª);
 	glEnableVertexAttribArray(0);
-	// ½âÊÍVertexÊı¾İ£¨Î»ÖÃ£¬Á½¸öÊı¾İÒ»¸öµã£¬GLFloatÀàĞÍ£¬½ûÓÃNormalize£¬Ö®ºó½âÊÍ£¬Ö®ºó½âÊÍ£©
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 5, 0);
+	// è§£é‡ŠVertexæ•°æ®ï¼ˆä½ç½®ï¼Œä¸¤ä¸ªæ•°æ®ä¸€ä¸ªç‚¹ï¼ŒGLFloatç±»å‹ï¼Œç¦ç”¨Normalizeï¼Œä¹‹åè§£é‡Šï¼Œä¹‹åè§£é‡Šï¼‰
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);
 	// Enable Second Attribute of the vertex
 	glEnableVertexAttribArray(1);
 	// tell openGL how to interpret data
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (char*)(sizeof(float) * 2));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, (char*)(sizeof(float) * 3));
 
 	// ELEMENT ARRAY
 	// Define indices
-	GLushort indices[] = { 0,1,2,0,3,4 };
+	GLushort indices[] = { 0,1,2,3,4,5 };
 	// Create a index buffer ID
 	GLuint indexBufferID;
 	// Create a index buffer
@@ -61,13 +65,12 @@ bool checkStatus(GLuint objectID, PFNGLGETSHADERIVPROC objectPropertyGetter, PFN
 	if (status != GL_TRUE)
 	{
 		GLint infoLogLength;
-		objectPropertyGetter(objectID, statusType, &infoLogLength);
+		objectPropertyGetter(objectID, GL_INFO_LOG_LENGTH, &infoLogLength);
 		GLchar* buffer = new GLchar[infoLogLength];
 
 		GLsizei bufferSize;
 		getInfoLogFunc(objectID, infoLogLength, &bufferSize, buffer);
 		std::cout << buffer << std::endl;
-
 		delete[] buffer;
 		return false;
 	}
@@ -80,8 +83,22 @@ bool checkShaderStatus(GLuint shaderID)
 }
 
 bool checkProgramStatus(GLuint programID)
-{
+{	
 	return checkStatus(programID, glGetProgramiv, glGetProgramInfoLog, GL_LINK_STATUS);
+}
+
+ std::string readShaderCode(const char* fileName)
+{
+	 std::ifstream myInput(fileName);
+	 if (!myInput.good())
+	 {
+		 std::cout << "File failed to load... " << fileName << std::endl;
+		 system("pause");
+		 exit(0);
+	 }
+	 return std::string(
+		 std::istreambuf_iterator<char>(myInput),
+		 std::istreambuf_iterator<char>());
 }
 
 void installShaders()
@@ -92,10 +109,12 @@ void installShaders()
 	
 	// Create a char pointer to read the shader string
 	const GLchar* adapter[1];
-	adapter[0] = vertexShaderCode;
+	std::string temp = readShaderCode("VertexShaderCode.glsl");
+	adapter[0] = temp.c_str();
 	// 0 means auto calulate when the compiler see a /0 null terminator
 	glShaderSource(vertexShaderID, 1, adapter, 0);
-	adapter[0] = fragmentShaderCode;
+	temp = readShaderCode("FragmentShaderCode.glsl");
+	adapter[0] = temp.c_str();
 	glShaderSource(fragmentShaderID, 1, adapter, 0);
 
 	glCompileShader(vertexShaderID);
@@ -121,6 +140,8 @@ void MyGLWindow::initializeGL()
 	// Init glew
 	glewInit();
 
+	glEnable(GL_DEPTH_TEST);
+
 	sendDataToOpenGL();
 
 	installShaders();
@@ -128,9 +149,10 @@ void MyGLWindow::initializeGL()
 
 void MyGLWindow::paintGL()
 {
+	glClear(GL_DEPTH_BUFFER_BIT);
 	// Set Viewport Width, Height
 	glViewport(0, 0, width(), height());
-	// ¿ªÊ¼»æÖÆ£¨»æÖÆÀàĞÍ£¬µÚÒ»¸öÊı¾İ£¬¶àÉÙ¸övertexäÖÈ¾£©
+	// å¼€å§‹ç»˜åˆ¶ï¼ˆç»˜åˆ¶ç±»å‹ï¼Œç¬¬ä¸€ä¸ªæ•°æ®ï¼Œå¤šå°‘ä¸ªvertexæ¸²æŸ“ï¼‰
 	//glDrawArrays(GL_TRIANGLES, 0, 40);
 	// Draw ELEMENT ARRAY
 	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);
